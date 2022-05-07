4e367ab509b7a20c2c2925e1880e252cb0068f8f
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2022-05-07 16:19:45 -0700

    add page

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import { Link } from "gatsby";
+import React from "react";
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About Me</title>
+            <p>Hi there, I created this!</p>
+            <Link to='/'>Home</Link>
+        </main>
+    );
+};
+
+export default AboutPage;
