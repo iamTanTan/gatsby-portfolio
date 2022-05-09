a8837e092c1eddcd7cc91efba4118474663d404e
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2022-05-09 14:24:08 -0700

    try to add tailwind

---
+++ src/components/Layout.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export const Layout: React.FC<{}> = ({ children }) => {
+    const linkStyle = `
+     Hover:opacity-70
+     text-sm
+     sm:text-lg
+    `;
+    const logoStyle = `
+    text-white 
+    font-rammetto
+    sm:text-base
+    text-sm
+    `;
+    return (
+        <div>
+            <nav className='flex sm:justify-between justify-around items-center bg-green-900 lg:px-20 sm:px-6 py-8 text-gray-100'>
+                <h3 className={logoStyle}>HealthRoom</h3>
+                <ul className='flex'>
+                    <li className={linkStyle}>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li className={linkStyle}>
+                        <Link to='/contact'>Contact</Link>
+                    </li>
+                    <li className={linkStyle}>
+                        <Link to='/about'>About</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <main>{children}</main>
+
+            <footer className='text-center py-8 bg-green-900 text-gray-100'>
+                <p>
+                    Copyright 2030 <span className={logoStyle}>HealthRoom</span>
+                </p>
+            </footer>
+        </div>
+    );
+};
